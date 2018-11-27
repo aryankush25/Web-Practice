9d0930ffc921dcde2e0ddeb2536d22ae1349ef38
Aryan Agarwal <aryankush025@gmail.com>
2018-11-27 23:40:03 +0530

    Color Game Randome color added and heading color changed

--- Color Game/colorGame.js
+++ Color Game/colorGame.js
@@ -1,34 +1,63 @@
 window.onload = function() {
-    var color = [
-        "rgb(255, 0, 0)",
-        "rgb(255, 255, 0)",
-        "rgb(0, 255, 0)",
-        "rgb(0, 255, 255)",
-        "rgb(0, 0, 255)",
-        "rgb(255, 0, 255)"
-    ];
+    var colors = generateRandomeColors(6);
     
     var squares = document.querySelectorAll(".square");
-    var pickedColor = color[3];
+    var pickedColor = pickColor();
     var colorDisplay = document.getElementById("colorDisplay");
+    var messageDisplay = document.querySelector("#message");
+    var h1 = document.getElementsByTagName("h1")[0];
     
     colorDisplay.textContent = pickedColor;
     
     for(let i = 0; squares.length; i++) {
-        //squares[i].setAttribute("style", "background-color: ${squares[i]}");
-        squares[i].style.backgroundColor = color[i];
+        //squares[i].setAttribute("style", "backgroundColor: ${squares[i]}");
+        squares[i].style.backgroundColor = colors[i];
         
         squares[i].addEventListener("click", function() {
             //alert("clicked " + (i + 1) + " square");
             var clickedColor = this.style.backgroundColor;
             
             if (clickedColor === pickedColor) {
-                alert("You Won !!!");
+                messageDisplay.textContent = "Correct";
+                changeColors(clickedColor);
+                h1.style.backgroundColor = clickedColor;
             } else {
-                this.style.backgroundColor = document.querySelector("body").style.backgroundColor;
+                this.style.backgroundColor = document.body.style.backgroundColor;
+                messageDisplay.textContent = "Try Again";
             }
-            
-            
         });
     }
+    
+    function changeColors(color) {
+        for(let i = 0; i < squares.length; i++) {
+            squares[i].style.backgroundColor = color;
+        }
+    }
+    
+    function pickColor() {
+        let x = Math.floor(Math.random() * colors.length);
+
+        return colors[x];
+    }
+    
+    function generateRandomeColors(length) {
+        var arr = [];
+        
+        for(let i = 0; i < length; i++) {
+            arr.push(randomeColor());
+        }
+        
+        return arr;
+    }
+    
+    function randomeColor() {
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        
+        let randColor = "rgb(" + r + ", " + g + ", " + b + ")";
+        
+        return randColor;
+    }
+    
 }
